55451b35230acb94aa4fede358e39bde677dddb8
Aishwaryasm1997 <110146436+Aishwaryasm1997@users.noreply.github.com>
2022-08-25 12:10:27 +0530

    Welcome to Employee Wage Computation

---
+++ EmpWage_UC1.java
@@ -0,0 +1,27 @@
+package bl.com.java;
+
+public class EmpWage_UC1 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		int num;
+		int min = 0;
+		int max = 5;
+		num = (int) (Math.random () );
+		
+		if (num <=1)
+		{
+		System.out.println("Employee is present");	
+		}
+		else if (num < 1)
+		{
+			System.out.println("Employee is Absent");
+		}
+		else
+		{
+			System.out.println( "" );
+		}
+	}
+}
+
