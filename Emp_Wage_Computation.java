5a653638b3a21b0314ab42d715227766f71dd4b8
Aishwaryasm1997 <110146436+Aishwaryasm1997@users.noreply.github.com>
2022-09-01 15:30:31 +0530

    Add files via upload

---
+++ Emp_Wage_Computation.java
@@ -0,0 +1,37 @@
+package bl.com.java;
+
+public class Emp_Wage_Computation {
+	public static final int FULL_TIME = 2;
+	public static final int PART_TIME = 1;
+	public static final int WAGE_PER_HR = 20;
+	public static final int WORKING_DAYS = 20;
+	public static final int MAX_WORKING_HRS = 100;
+
+	public static void main(String[] args) {
+		int empHrs = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+
+		while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < WORKING_DAYS) {
+			totalWorkingDays++;
+
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+			switch (empCheck) {
+			case PART_TIME:
+				empHrs = 4;
+				break;
+			case FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 0;
+			}
+
+			totalEmpHrs += empHrs;
+			System.out.println("For Day" + totalWorkingDays + " : Employee Maximum Working Hours are " + empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * WAGE_PER_HR;
+		System.out.println("Total Employee Wage for month is " + totalEmpWage);
+	}
+}
